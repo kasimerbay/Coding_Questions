a9fbff5cb28824a8b4747a01172c3205607223b8
Ahmet Kasim Erbay <kasim.erbay@boun.edu.tr>
2022-10-05 09:37:38 +0300

    simpleMathTest updated

--- Questions/MateAcademy/simpleMathTest.js
+++ Questions/MateAcademy/simpleMathTest.js
@@ -1,9 +1,11 @@
 function isPositive(number){
   return number > 0 ? true:false;
 }
+
 function isEven(number){
   return number%2 === 0 ? true:false;
 }
+
 function isDivisible10(number){
   return number%10 === 0 ? true:false;
 }
